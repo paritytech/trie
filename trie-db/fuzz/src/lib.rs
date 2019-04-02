b3c988c66c0a0c8fd8a10c2612587ddc086d8370
cheme <emericchevalier.pro@gmail.com>
2019-04-02 21:06:11 +0200

    Reset fuzzer to right no_ext impl, found a failing test (in comment).

--- trie-db/fuzz/src/lib.rs
+++ trie-db/fuzz/src/lib.rs
@@ -136,20 +136,24 @@ pub fn fuzz_that_no_ext_insert(input: &[u8]) {
 
 pub fn fuzz_that_no_ext_insert_remove(input: &[u8]) {
 	let data = fuzz_to_data(input);
+	println!("data{:?}", data);
+  // TODO debug this failing:
+//data[([0], [0, 251]), ([0], [251, 255]), ([251, 255, 79, 59, 42, 34, 0, 0, 0, 255, 255, 255, 255, 255, 255], [255, 0]), ([255, 0, 0, 0, 91, 0, 0, 0, 0, 82, 0, 59, 136, 136, 0], [0, 0]), ([0], [0, 0]), ([0], [0, 0]), ([0], [0, 0]), ([0], [0, 0]), ([0], [0, 0]), ([0], [0, 0]), ([0], [0, 0])]
+
 	let mut data2 = std::collections::BTreeMap::new();
 	let mut memdb = MemoryDB::<_, PrefixedKey<_>, _>::default();
 	let mut root = Default::default();
 	let mut torem = None;
 	let mut a = 0;
 	{
-		let mut t = RefTrieDBMut::new(&mut memdb, &mut root);
+		let mut t = RefTrieDBMutNoExt::new(&mut memdb, &mut root);
 		t.commit();
 	}
 //	println!("data{:?}", data);
 	while a < data.len() {
 
 		root = {
-		let mut t = RefTrieDBMut::from_existing(&mut memdb, &mut root).unwrap();
+		let mut t = RefTrieDBMutNoExt::from_existing(&mut memdb, &mut root).unwrap();
 		for _ in 0..3 {
 		if a % 7 == 6	{
 //	println!("remrand{:?}", a);
@@ -180,9 +184,8 @@ pub fn fuzz_that_no_ext_insert_remove(input: &[u8]) {
 		*t.root()
 		};
 	}
-	let mut t = RefTrieDBMut::from_existing(&mut memdb, &mut root).unwrap();
+	let mut t = RefTrieDBMutNoExt::from_existing(&mut memdb, &mut root).unwrap();
 	// we are testing the RefTrie code here so we do not sort or check uniqueness
 	// before.
-	//println!("data{:?}", data);
-	assert_eq!(*t.root(), calc_root(data2));
+	assert_eq!(*t.root(), calc_root_no_ext(data2));
 }
