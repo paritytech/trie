e1d042b33fc780093d98c52e576d64b106771637
Sergey Pepyakin <sergei@parity.io>
2019-07-29 17:26:47 +0200

    Remove core-intrinsics

--- hash-db/src/lib.rs
+++ hash-db/src/lib.rs
@@ -13,8 +13,8 @@
 // limitations under the License.
 
 //! Database of byte-slices keyed to their hash.
+
 #![cfg_attr(not(feature = "std"), no_std)]
-#![cfg_attr(not(feature = "std"), feature(core_intrinsics))]
 
 #[cfg(feature = "std")]
 use std::fmt::Debug;
