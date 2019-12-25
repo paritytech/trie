37fbf615d94a76a8f09f0e293002a5cfb1a64346
NikVolf <nikvolf@gmail.com>
2019-12-25 15:41:20 +0300

    fix stray tab

--- trie-db/src/trie_codec.rs
+++ trie-db/src/trie_codec.rs
@@ -32,7 +32,7 @@ use crate::{
 	nibble_ops::NIBBLE_LENGTH, node::{Node, NodeHandle, NodeHandlePlan, NodePlan, OwnedNode},
 };
 
-use	crate::core_::{
+use crate::core_::{
 	convert::TryInto,
 	marker::PhantomData,
 	result,
