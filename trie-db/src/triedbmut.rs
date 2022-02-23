84e30086693e1d8fa3a85a5d2453bcde39b3f316
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2022-02-23 11:43:11 +0100

    Use ToHex
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- trie-db/src/triedbmut.rs
+++ trie-db/src/triedbmut.rs
@@ -1635,7 +1635,7 @@ where
 					}
 				});
 				#[cfg(feature = "std")]
-				trace!(target: "trie", "encoded root node: {:x?}", &encoded_root[..]);
+				trace!(target: "trie", "encoded root node: {:?}", ToHex(&encoded_root[..]));
 
 				*self.root = self.db.insert(EMPTY_PREFIX, &encoded_root[..]);
 				self.hash_count += 1;
@@ -1752,7 +1752,7 @@ where
 		let mut old_val = None;
 
 		#[cfg(feature = "std")]
-		trace!(target: "trie", "insert: key={:x?}, value={:?}", key, ToHex(&value));
+		trace!(target: "trie", "insert: key={:?}, value={:?}", ToHex(key), ToHex(&value));
 
 		let root_handle = self.root_handle();
 		let (new_handle, _changed) =
@@ -1767,7 +1767,7 @@ where
 
 	fn remove(&mut self, key: &[u8]) -> Result<Option<Value<L>>, TrieHash<L>, CError<L>> {
 		#[cfg(feature = "std")]
-		trace!(target: "trie", "remove: key={:x?}", key);
+		trace!(target: "trie", "remove: key={:?}", ToHex(key));
 
 		let root_handle = self.root_handle();
 		let mut key_slice = NibbleSlice::new(key);
