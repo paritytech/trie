11a934cdfd147eb20e4f91d2d7aff244f87dcc85
Marcin S <marcin@bytedude.com>
2022-11-29 10:48:06 -0500

    memory-db: remove parity-util-mem (#172)

--- trie-db/test/src/iter_build.rs
+++ trie-db/test/src/iter_build.rs
@@ -78,7 +78,7 @@ fn compare_implementations_prefixed(data: Vec<(Vec<u8>, Vec<u8>)>) {
 }
 fn compare_implementations_prefixed_internal<T: TrieLayout>(data: Vec<(Vec<u8>, Vec<u8>)>) {
 	let memdb = MemoryDB::<_, PrefixedKey<_>, _>::default();
-	let hashdb = MemoryDB::<T::Hash, PrefixedKey<_>, DBValue, _>::default();
+	let hashdb = MemoryDB::<T::Hash, PrefixedKey<_>, DBValue>::default();
 	reference_trie::compare_implementations::<T, _>(data, memdb, hashdb);
 }
 fn compare_implementations_h(data: Vec<(Vec<u8>, Vec<u8>)>) {
@@ -89,12 +89,12 @@ fn compare_implementations_h(data: Vec<(Vec<u8>, Vec<u8>)>) {
 }
 fn compare_implementations_h_internal<T: TrieLayout>(data: Vec<(Vec<u8>, Vec<u8>)>) {
 	let memdb = MemoryDB::<_, HashKey<_>, _>::default();
-	let hashdb = MemoryDB::<T::Hash, HashKey<_>, DBValue, _>::default();
+	let hashdb = MemoryDB::<T::Hash, HashKey<_>, DBValue>::default();
 	reference_trie::compare_implementations::<T, _>(data.clone(), memdb, hashdb);
 }
 fn compare_implementations_no_extension_unordered(data: Vec<(Vec<u8>, Vec<u8>)>) {
 	let memdb = MemoryDB::<_, HashKey<_>, _>::default();
-	let hashdb = MemoryDB::<RefHasher, HashKey<_>, DBValue, _>::default();
+	let hashdb = MemoryDB::<RefHasher, HashKey<_>, DBValue>::default();
 	reference_trie::compare_implementations_unordered::<NoExtensionLayout, _>(data, memdb, hashdb);
 }
 fn compare_insert_remove<T: TrieLayout>(data: Vec<(bool, Vec<u8>, Vec<u8>)>) {
